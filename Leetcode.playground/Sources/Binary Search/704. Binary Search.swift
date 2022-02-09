26833d02df11acb35c60842add6f33cb6045d9d7
Yury Lugovoy <lugovoy@kontur.ru>
2022-02-09 19:34:13 +0500

    Binary search

---
+++ Leetcode.playground/Sources/Binary Search/704. Binary Search.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+// https://leetcode.com/problems/binary-search/
+public enum BinarySearch {
+  public static func searchV1(_ nums: [Int], _ target: Int) -> Int {
+    var mid: Int = 0
+    var leftIndex = 0
+    var rightIndex = nums.count - 1
+    
+    while leftIndex <= rightIndex {
+      mid = (leftIndex + rightIndex) / 2
+      let value = nums[mid]
+      if target == value {
+        return mid
+      } else if target < nums[mid] {
+        rightIndex = mid - 1
+      } else {
+        leftIndex = mid + 1
+      }
+    }
+    return -1
+  }
+}
