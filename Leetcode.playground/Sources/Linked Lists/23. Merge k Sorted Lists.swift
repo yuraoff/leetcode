870472f32ec5ca4e89762111a1a3e259db7b96a9
Yury Lugovoy <lugovoy@kontur.ru>
2022-02-09 11:08:31 +0500

    LinkedLists. Merge K Sorted Lists V1

---
+++ Leetcode.playground/Sources/Linked Lists/23. Merge k Sorted Lists.swift
@@ -0,0 +1,27 @@
+import Foundation
+
+enum MergeKsortedLists {
+  func mergeKListsV1(_ lists: [ListNode?]) -> ListNode? {
+    var numbers = [Int]()
+    for head in lists {
+      var currentNode: ListNode? = head
+      while let current = currentNode {
+        numbers.append(current.val)
+        currentNode = current.next
+      }
+    }
+    numbers.sort()
+    var head: ListNode?
+    var lastNode: ListNode?
+    for number in numbers {
+      let current = ListNode(number)
+      if head == nil {
+        head = current
+      } else {
+        lastNode?.next = current
+      }
+      lastNode = current
+    }
+    return head
+  }
+}
