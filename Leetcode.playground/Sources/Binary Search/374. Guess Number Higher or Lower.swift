e0742238f419b18a4262f8ccdea1c88aafb73b84
Yury Lugovoy <lugovoy@kontur.ru>
2022-02-09 20:30:18 +0500

    Binary Search. Guess Number Higher or Lower

---
+++ Leetcode.playground/Sources/Binary Search/374. Guess Number Higher or Lower.swift
@@ -0,0 +1,35 @@
+import Foundation
+
+// https://leetcode.com/problems/guess-number-higher-or-lower/
+public enum GuessNumberHigherOrLower {
+  
+  public static func guessNumberV1(_ n: Int) -> Int {
+    var leftIndex = 0
+    var rightIndex = n
+    
+    while leftIndex <= rightIndex {
+      let mid = (leftIndex + rightIndex) / 2
+      let guessResult = guess(mid)
+      if guessResult == 0 {
+        return mid
+      } else if guessResult == -1 {
+        rightIndex = mid - 1
+      } else {
+        leftIndex = mid + 1
+      }
+    }
+    
+    return -1
+  }
+  
+  private static func guess(_ num: Int) -> Int  {
+    let target = 1 // Your secret value
+    if num == target {
+      return 0
+    } else if target > num {
+      return 1
+    } else {
+      return -1
+    }
+  }
+}
--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 - [23. Merge k Sorted Lists](./Leetcode.playground/Sources/Linked%20Lists/23.%20Merge%20k%20Sorted%20Lists.swift)
 - [141. Linked List Cycle](./Leetcode.playground/Sources/Linked%20Lists/141.%20Linked%20List%20Cycle.swift)
 - [206. Reverse Linked List](./Leetcode.playground/Sources/Linked%20Lists/206.%20Reverse%20Linked%20List.swift)
+
+## Binary Search
+- [374. Guess Number Higher or Lower](./Leetcode.playground/Sources/Binary%20Search/374.%20Guess%20Number%20Higher%20or%20Lower.swift)
+- [704. Binary Search](./Leetcode.playground/Sources/Binary%20Search/704.%20Binary%20Search.swift)
